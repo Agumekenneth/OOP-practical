86fbca5c326fd57cca8392a7c6cb578ab1306c5e
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-09-14 17:10:47 +0300

    yeah

--- practice.py
+++ practice.py
@@ -70,13 +70,43 @@
 
 # print(y)
 
+# x = int(input())
+# y = int(input())
+
+# x = x / y
+# y = y / x
+
+# print(y)
+
+# x = int(input())
+# y = int(input())
+
+# x = x % y
+# x = x % y
+# y = y % x
+
+# print(y)
+
+# x = input()
+# y = int(input())
+
+# print(x * y)
+
+# x = 1 / 2 + 3 // 3 + 4 ** 2
+# print(x)
+
 x = int(input())
 y = int(input())
 
-x = x / y
-y = y / x
+print(x + y)
 
-print(y)
+
+
+
+
+
+
+
 
 
 
