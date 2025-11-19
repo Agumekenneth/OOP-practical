b7a4449259f74ec8dc17334f21eb6c5a17cc53cf
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-11-19 12:26:54 +0300

    init

---
+++ testprep.py
@@ -0,0 +1,6 @@
+class StudentBorrowing:
+    def __init__(self, title, borrower_name, date_borrowed, status):
+        self.title = title
+        self.borrower_name = borrower_name
+        self.date_borrowed = date_borrowed
+        self.statius = status
