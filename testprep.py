8fa8332e995880e2a85d2a6685d80bc2d810ea21
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-11-20 10:35:55 +0300

    prac

--- testprep.py
+++ testprep.py
@@ -4,11 +4,25 @@ class BorrowRecord:
         self._borrowerName = borrowerName
         self._dateBorrowed = dateBorrowed
         self._status = status
-        self.copies = {}
+    
+    # title Validation
     def _set_title(self,title):
         if title.strip() =="":
             raise ValueError("Title cannot be empty")
         self.title = title
 
+    # Getters(Encapsulation)
+    def get_title(self):
+        return self.title
+    def get_borrowerName(self):
+        return self._borrowerName
+    def get_dateBorrowed(self):
+        return self._dateBorrowed
+    def get_status(self):
+        return self._status
+    
+    # Mark as returned (behavior)
+    def marked_returned(self):
+        self._status ="Returned"
     
     
