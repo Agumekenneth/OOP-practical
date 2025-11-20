42f411941fdd6afe9b11035a3ee414f237af472e
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-11-20 10:55:33 +0300

    prac

--- testprep.py
+++ testprep.py
@@ -1,19 +1,20 @@
+from datetime import date
 class BorrowRecord:
     def __init__(self, title, borrowerName, dateBorrowed, status):
-        self.title = title
+        self._set_title(title)              
         self._borrowerName = borrowerName
         self._dateBorrowed = dateBorrowed
         self._status = status
     
-    # title Validation
-    def _set_title(self,title):
-        if title.strip() =="":
+    # title Validation (abstraction)
+    def _set_title(self, title):
+        if title.strip() == "":
             raise ValueError("Title cannot be empty")
-        self.title = title
+        self._title = title
 
-    # Getters(Encapsulation)
+    # Getters (encapsulation + abstraction)
     def get_title(self):
-        return self.title
+        return self._title
     def get_borrowerName(self):
         return self._borrowerName
     def get_dateBorrowed(self):
@@ -23,6 +24,14 @@ class BorrowRecord:
     
     # Mark as returned (behavior)
     def marked_returned(self):
-        self._status ="Returned"
-    
-    
+        self._status = "Returned"
+class Library:
+    def __init__(self):
+        self._records = [] # holds BorrowRecord objects
+    def add_record(self, record):
+        self._records.append(record)
+
+    # print summary 
+    def print_summary(self):
+        today = date.today().isoformat()
+        today_count = sum(1 for r in self.records if r.get_date() == today)
