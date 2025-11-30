f12121cb744d15f8a4fb412257f8bba9d41d1f92
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-11-30 13:49:12 +0300

    yeah exams init

--- exam.py
+++ exam.py
@@ -16,4 +16,70 @@ class BankAccount:
         if amount >=0:
             self._balance = amount
         else:
-            
+            print(f"Balance can't be negative")
+
+    @abstractmethod
+    def deposit(self,amount):
+        pass
+    @abstractmethod
+    def withdraw(self,amount):
+        pass
+    @abstractmethod
+    def check_balance(self, amount):
+        pass
+class SavingsAccount(BankAccount):
+    def __init__(self, account_number, balance=0, interest_rate = 0.05):
+        super().__init__(account_number, balance)
+        self._interest_rate = interest_rate
+    def depsoit(self,amount):
+        if amount >0:
+            self.balance += amount
+            print(f"Deposited{amount}, to savings account{self.account_number}, new balance is UGX{self.balance}")
+        else:
+            print(f"Invalid deposit amount please!!")
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            print(f" withdrew amount UGX{amount} from account{self.account_number}and yoir new balnace is UGX{self.balance}")
+        else:
+            print(f"Invalid withdrew")
+    def check_balance(self):
+        print(f" Your savings account{self.account_number} has a balance of UGX{self.balance} please!!!")
+    def calculate_interest(self):
+        interest = self.balance * self._interest_rate
+        self.deposit(interest)
+        print(f"Interest of UGX{interest} has been addded to account{self.account_number}, your new balance is UGX{self.balance}")
+class CheckingAccount(BankAccount):
+    def __init__(self, account_number, balance =0,overdraft_limit=50000):
+        self._overdraft_limit = overdraft_limit
+        super().__init__(account_number,balance)
+    def deposit(self,amount):
+        if amount >0:
+            self.balance += amount
+            print(f"depsoited UGX{amount} from checking account {self.account_number} and your new balance is UGx{self.balance}")
+        else:
+            print(f'Invalid deposit')
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance + self._overdraft_limit:
+            self.balance -= amount
+            print(f"withdrew amount UGX{amount} from checking account number{self.account_number} and your new balance is {self.balance}")
+        else:
+            print(f"Insufficient Funds, amount exceeds overdraft limit")
+    def check_balance(self):
+        print(f"checking account{self.account_number} has a balance of UGX{self.balance}")
+class BankSystem:
+    def __init__(self):
+        self.accounts = []
+    def create_account(self):
+        account_type = input("Enter account type please!!(savings/checking)").strip().lower()
+        account_number = input("Enter account number").strip()
+        if account_number in self.accounts:
+            print(f"Account already exists")
+            return
+        if account_type == 'savings':
+            self.accounts[account_number] = SavingsAccount(account_number)
+        elif account_type == 'checking':
+            account_type[account_number] = CheckingAccount(account_number)
+        else:
+            print(f"Invalid account type, choose either 'savings' or 'checking' accounts.")
+        print(f"{account_type.capitalize()}account {account_number} has been created ")
